e2c3d5d183b69ef456af74d7400068c2b842b7b2
LakshmiPravallika <github.pravallika@gmail.com>
2016-12-26 15:25:12 +0530

    hhieeeeeeee

---
+++ src/com/phone/Phone.java
@@ -0,0 +1,9 @@
+package com.phone;
+
+public interface Phone {
+	abstract public void call();
+	abstract public void message();
+	
+	
+
+}
---
+++ src/com/phone/ReguralPhone.java
@@ -0,0 +1,25 @@
+package com.phone;
+
+public class ReguralPhone implements Phone {
+	
+	public ReguralPhone()
+	{
+		
+		
+	}
+	
+	
+	public void call()
+	{
+		System.out.println("calling....");
+	}
+	public void message()
+	{
+		System.out.println("messaging...");
+	}
+	public void radio()
+	{
+		System.out.println("radio is playing.....");
+	}	
+	
+}
